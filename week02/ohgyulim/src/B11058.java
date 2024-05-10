55e28a40fbf3fed84f7edfe2cd58368a2d9dfd3a
gyulim5 <ohrbfla1004@khu.ac.kr>
2024-05-10 14:34:34 +0900

    [BOJ]11058/크리보드/골드5

--- week02/ohgyulim/src/B11058.java
+++ week02/ohgyulim/src/B11058.java
@@ -7,8 +7,7 @@ public class B11058 {
 		int n = sc.nextInt();
 		long[] dp = new long[n + 1];
 
-		dp[1] = 1;
-		for (int i = 2; i <= n; i++) {
+		for (int i = 1; i <= n; i++) {
 			dp[i] = dp[i - 1] + 1;
 			if (i <= 6)
 				continue;
@@ -18,8 +17,19 @@ public class B11058 {
 		}
 		// dp[4] = aaaa -> 4
 		// dp[5] = aaaaa -> 5
-		// dp[6] = aa acv v -> 6
-		// dp[7] = aaa acv v -> 9
+		// dp[6] = aa acv v = dp[2] * 3 -> 6
+		// dp[7] = aaa acv v = dp[3] * 3 -> 9
+
+		// dp[1] = a
+		// dp[2] = aa
+		// dp[3] = aaa
+
+		// dp[i - 4] * 3 == acv v -> v 2개 = 3배: src + (src) * 2
+		// dp[i - 5] * 4 == acv v v -> v 3개 = 4배: src + (src) * 3
+		// dp[i - 6] * 5 == acv v v v -> v 4개 = 5배: src + (src) * 4
+
+		//dp[7] = Math.max(dp[3] * 3, dp[2] * 4, dp[1] * 5)
+
 		System.out.println(dp[n]);
 	}
 }
